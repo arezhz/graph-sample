99c5383a870ac5bfb7d6a735bc074cfda41fd174
arezhz <arezhz@gmail.com>
2024-03-09 15:17:57 +0330

    install cytoscape-leaf package

--- src/app/components/graph-leaflet.ts
+++ src/app/components/graph-leaflet.ts
@@ -1,6 +1,18 @@
+import { UtilityService } from "../services/utility";
+import leaflet from 'cytoscape-leaf';
+
 export default customElements.define(
   "graph-leaflet",
   class extends HTMLElement {
+    cy!: cytoscape.Core;
+    utlityService;
+
+    constructor() {
+      super();
+      this.utlityService = new UtilityService();
+      cytoscape.use( leaflet );
+    }
+
     connectedCallback() {
       const container = document.createElement("div");
       container.setAttribute("id", "testttttt");
